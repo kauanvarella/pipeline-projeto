d924d4481ce3b86b234b6b743937dd42150ecd23
kauanvarella <kauanvarella@gmail.com>
2022-07-28 07:17:35 -0300

    ...

--- pipeline-prod/infra-prod/ami.tf
+++ pipeline-prod/infra-prod/ami.tf
@@ -1,5 +1,5 @@
 resource "aws_ami_from_instance" "AMI_Prod" {
   name = "imagem-producao"
-  source_instance_id = module.app-homolog.aws_instance.app_server.id
+  source_instance_id = module.aws-homolog.aws_instance.app_server.id
   snapshot_without_reboot = true
 } 
